af14856e4e9bf1c4ac3eaf301f76b90f4c9a65c3
Mangawy <eng.mohamed.hosaam@gmail.com>
2016-05-22 17:45:52 +0200

    update model

--- app/model/Model.php
+++ app/model/Model.php
@@ -73,24 +73,6 @@
 			return $row[0]===$hash;
 		}
 		
-		
-		
-		public function is_email_exist($email)
-		{
-			$email=mysqli_real_escape_string($this->db,$email);
-			$query="SELECT Email FROM user WHERE Email='{$email}'";
-			$result=mysqli_query($this->db,$query);
-			if(!$result)
-			{
-				die("query failed "." ".mysqli_error($this->db));	
-			}
-			if(mysqli_num_rows($result)!=0)
-			{
-				return true;
-			}
-			return false;
-		}
-		
 		public function get_problems($fillter,$offset=-1)
 		{
 			$query="SELECT * FROM problem ";
@@ -201,6 +183,48 @@
 			}
 			return $result;
 		}
+		public function get_contests($fillter,$offset=-1)
+		{
+			$query="SELECT * FROM contest WHERE 1 ";
+			if(isset($_GET["Access"]))
+			{
+				if($_GET["Access"]!="All"){
+					$query.=" AND type='{$_GET["Access"]}'";
+				}
+			}
+			if(isset($_GET["status"]))
+			{
+				if($_GET["status"]!="All")
+				{
+					$t=time();
+					if($_GET["status"]=="Coming"){$query.=" AND start>{$t}";}
+					if($_GET["status"]=="Running"){$query.=" AND start<={$t} AND end>={$t}";}
+					if($_GET["status"]=="Passed"){$query.=" AND end<{$t}";}
+				}
+			}
+			if(isset($_GET["Fcontest"]))
+			{
+				$str=$_GET['Fcontest'];
+				$change = array(' '=>'%',);
+				$s=strtr(mysqli_real_escape_string($this->db,$str),$change);
+				$query .= " AND name LIKE '%".$s."%'";
+			}
+			if(isset($_GET["Fuser"]))
+			{					
+				$str=$_GET['Fuser'];
+				$change = array(' '=>'%',);
+				$s=strtr(mysqli_real_escape_string($this->db,$str),$change);
+				$query .= " AND creator_Handle LIKE '%".$s."%'";
+			}
+			
+			if($offset!=-1)$query .=" ORDER BY contest_id DESC LIMIT 20 OFFSET {$offset} ";
+			$result=mysqli_query($this->db,$query);
+			if(!$result)
+			{
+				die("query failed "." ".mysqli_error($this->db));	
+			}
+			return $result;
+		}
 		
 		public function get_category()
 		{
@@ -220,7 +244,21 @@
 			return $res;
 		}
 		
-		
+		public function is_email_exist($email)
+		{
+			$email=mysqli_real_escape_string($this->db,$email);
+			$query="SELECT Email FROM user WHERE Email='{$email}'";
+			$result=mysqli_query($this->db,$query);
+			if(!$result)
+			{
+				die("query failed "." ".mysqli_error($this->db));	
+			}
+			if(mysqli_num_rows($result)!=0)
+			{
+				return true;
+			}
+			return false;
+		}
 		
 		public function count_accepted_submission($problem_id)
 		{
@@ -237,7 +275,6 @@
 			$r=mysqli_fetch_array($res);
 			return $r[0];
 		}
-		
 		
 		public function get_problem($problem_id)
 		{
@@ -259,6 +296,90 @@
 			$res=mysqli_query($this->db,$query);
 			$r=mysqli_fetch_array($res);
 			return $r[0];
+		}
+		
+		public function close_connection()
+		{
+			if(isset($this->db))
+			{
+				mysqli_close($this->db);
+				unset($this->db);
+			}
+		}
+		public function insert_submission($sub)
+		{
+			if(strlen($sub['Verdict'])==0)$sub['Verdict']="Judge Error";
+			$sub['Soultion']=mysqli_real_escape_string($this->db,$sub['Soultion']);
+			$sub['Verdict']=mysqli_real_escape_string($this->db,$sub['Verdict']);
+			$sub['Language']=mysqli_real_escape_string($this->db,$sub['Language']);
+			$query="INSERT INTO submission";
+					$query.="(
+							TIME,
+							Verdict,
+							Soultion, 		
+							Language,  
+							submission_date,
+							memory,
+							user_Handle, 
+							Problem_id
+						) ";
+					
+					$query.="VALUES(
+						 {$sub['TIME']},
+						'{$sub['Verdict']}', 		
+						'{$sub['Soultion']}',  
+						'{$sub['Language']}',
+						'{$sub['submission_date']}',
+						 {$sub['memory']},
+						'{$sub['user_Handle']}',
+						 {$sub['Problem_id']}
+					)";
+					if(!mysqli_query($this->db,$query))
+					{
+						die("query failed "." ".mysqli_error($this->db));
+					}
+		}
+		public function count_soulution($s)
+		{
+			$s=mysqli_real_escape_string($this->db,$s);
+			return mysqli_fetch_row(mysqli_query($this->db,"SELECT count(submission_id) FROM submission WHERE Soultion='{$s}'"))[0];
+		}
+		public function get_wating_submission()
+		{
+			$query="SELECT submission_id,from_oj FROM submission Join problem ON problem.problem_id=submission.Problem_id WHERE Verdict='Waiting'";
+			return mysqli_query($this->db,$query);
+		}
+		public function count_wating_submission($oj)
+		{
+			$query="SELECT count(submission_id) FROM submission Join problem ON problem.problem_id=submission.Problem_id WHERE Verdict='Waiting' AND from_oj='{$oj}'";
+			return mysqli_fetch_row(mysqli_query($this->db,$query))[0];
+		}
+		public function update_submission($sub_id,$sub)
+		{
+			$query="UPDATE submission ";
+			$query.="SET 
+					TIME={$sub['TIME']},
+					Verdict='{$sub['Verdict']}',
+					memory={$sub['memory']} 		
+					WHERE submission_id={$sub_id}";
+			if(!mysqli_query($this->db,$query))
+			{
+				die("query failed "." ".mysqli_error($this->db));
+			}
+		}
+		public function INSERT_contest()
+		{
+			$start=(strtotime($_POST["date"])+$_POST["hour"]*60*60+$_POST["min"]*60);
+			$end=(strtotime($_POST["Edate"])+$_POST["Ehour"]*60*60+$_POST["Emin"]*60);
+			$name=mysqli_real_escape_string($this->db,$_POST["contest_name"]);
+			$query="INSERT INTO contest";
+			$query.="(start,end,name,creator_Handle,type) ";
+			$query.="VALUES({$start},{$end},'{$name}','{$_COOKIE["user_handle"]}','{$_POST["type"]}')";
+			$result=mysqli_query($this->db,$query);
+			if(!$result)
+			{
+				die("query failed "." ".mysqli_error($this->db)); 
+			}
 		}
 		
 		public function insert_blog($data,$TAG)
@@ -461,126 +582,5 @@
 		{
 			return mysqli_real_escape_string($this->db,$str);
 		}
-		
-		
-		public function get_contests($fillter,$offset=-1)
-		{
-			$query="SELECT * FROM contest WHERE 1 ";
-			if(isset($_GET["Access"]))
-			{
-				if($_GET["Access"]!="All"){
-					$query.=" AND type='{$_GET["Access"]}'";
-				}
-			}
-			if(isset($_GET["status"]))
-			{
-				if($_GET["status"]!="All")
-				{
-					$t=time();
-					if($_GET["status"]=="Coming"){$query.=" AND start>{$t}";}
-					if($_GET["status"]=="Running"){$query.=" AND start<={$t} AND end>={$t}";}
-					if($_GET["status"]=="Passed"){$query.=" AND end<{$t}";}
-				}
-			}
-			if(isset($_GET["Fcontest"]))
-			{
-				$str=$_GET['Fcontest'];
-				$change = array(' '=>'%',);
-				$s=strtr(mysqli_real_escape_string($this->db,$str),$change);
-				$query .= " AND name LIKE '%".$s."%'";
-			}
-			if(isset($_GET["Fuser"]))
-			{					
-				$str=$_GET['Fuser'];
-				$change = array(' '=>'%',);
-				$s=strtr(mysqli_real_escape_string($this->db,$str),$change);
-				$query .= " AND creator_Handle LIKE '%".$s."%'";
-			}
-			
-			if($offset!=-1)$query .=" ORDER BY contest_id DESC LIMIT 20 OFFSET {$offset} ";
-			$result=mysqli_query($this->db,$query);
-			if(!$result)
-			{
-				die("query failed "." ".mysqli_error($this->db));	
-			}
-			return $result;
-		}
-		
-		
-		
-		
-		
-		
-		public function insert_submission($sub)
-		{
-			if(strlen($sub['Verdict'])==0)$sub['Verdict']="Judge Error";
-			$sub['Soultion']=mysqli_real_escape_string($this->db,$sub['Soultion']);
-			$sub['Verdict']=mysqli_real_escape_string($this->db,$sub['Verdict']);
-			$sub['Language']=mysqli_real_escape_string($this->db,$sub['Language']);
-			$query="INSERT INTO submission";
-					$query.="(
-							TIME,
-							Verdict,
-							Soultion, 		
-							Language,  
-							submission_date,
-							memory,
-							user_Handle, 
-							Problem_id
-						) ";
-					
-					$query.="VALUES(
-						 {$sub['TIME']},
-						'{$sub['Verdict']}', 		
-						'{$sub['Soultion']}',  
-						'{$sub['Language']}',
-						'{$sub['submission_date']}',
-						 {$sub['memory']},
-						'{$sub['user_Handle']}',
-						 {$sub['Problem_id']}
-					)";
-					if(!mysqli_query($this->db,$query))
-					{
-						die("query failed "." ".mysqli_error($this->db));
-					}
-		}
-		public function count_soulution($s)
-		{
-			$s=mysqli_real_escape_string($this->db,$s);
-			return mysqli_fetch_row(mysqli_query($this->db,"SELECT count(submission_id) FROM submission WHERE Soultion='{$s}'"))[0];
-		}
-		public function get_wating_submission()
-		{
-			$query="SELECT submission_id,from_oj FROM submission Join problem ON problem.problem_id=submission.Problem_id WHERE Verdict='Waiting'";
-			return mysqli_query($this->db,$query);
-		}
-		public function count_wating_submission($oj)
-		{
-			$query="SELECT count(submission_id) FROM submission Join problem ON problem.problem_id=submission.Problem_id WHERE Verdict='Waiting' AND from_oj='{$oj}'";
-			return mysqli_fetch_row(mysqli_query($this->db,$query))[0];
-		}
-		public function update_submission($sub_id,$sub)
-		{
-			$query="UPDATE submission ";
-			$query.="SET 
-					TIME={$sub['TIME']},
-					Verdict='{$sub['Verdict']}',
-					memory={$sub['memory']} 		
-					WHERE submission_id={$sub_id}";
-			if(!mysqli_query($this->db,$query))
-			{
-				die("query failed "." ".mysqli_error($this->db));
-			}
-		}
-		
-		public function close_connection()
-		{
-			if(isset($this->db))
-			{
-				mysqli_close($this->db);
-				unset($this->db);
-			}
-		}
-		
 	}
 ?>
